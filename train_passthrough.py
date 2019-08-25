4d417f6a2840d752aaee90878ed449efcef30beb
ben black <weepingwillowben@gmail.com>
2019-08-25 18:22:01 -0400

    added higherarchical structure, changed to NCHW

--- train_passthrough.py
+++ train_passthrough.py
@@ -1,16 +1,36 @@
 import os
-os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
+#os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import random
 import shutil
 
-FORMAT = 'NHWC'
+FORMAT = 'NCHW'
 BATCH_SIZE = 8
+
+IMG_SIZE = (200,320)
+
+def round_up_div(num,denom):
+    return (num+denom-1) // denom
+
+def get_out_dim(dim,level):
+    return round_up_div(dim, 2**(level-1))
+
+def get_out_shape(level):
+    return (get_out_dim(IMG_SIZE[0],level),get_out_dim(IMG_SIZE[1],level))
 
 def sqr(x):
     return x * x
+
+def unpool(tens4d,factor):
+    shape = tens4d.get_shape().as_list()
+    spread_shape = [shape[0],shape[1],1,shape[2],1,shape[3]]
+    reshaped = tf.reshape(tens4d,spread_shape)
+    tiled = tf.tile(reshaped,[1,1,factor,1,factor,1])
+    new_shape = [shape[0],shape[1],factor*shape[2],factor*shape[3]]
+    back = tf.reshape(tiled,new_shape)
+    return back
 
 class Dense:
     def __init__(self,input_dim,out_dim,activation):
@@ -43,7 +63,7 @@ class Conv2d:
         linval = tf.nn.conv2d(
             input=input_vec,
             filter=self.weights,
-            strides=[1]+self.strides+[1],
+            strides=self.strides,
             data_format=FORMAT,
             padding=self.padding)
         #affine_val = linval + self.biases
@@ -55,29 +75,40 @@ class Conv2d:
         return [self.weights,self.biases]
 
 
+def Conv1x1(input_dim,out_dim,activation):
+    return Conv2d(input_dim,out_dim,[1,1],activation)
+
+def Conv1x1Upsample(input_dim,out_dim,activation,out_shape,upsample_factor):
+    return ConvTrans2d(input_dim,out_dim,[1,1],activation,out_shape,strides=[upsample_factor,upsample_factor])
+
+
 class ConvTrans2d:
-    def __init__(self,input_dim,out_dim,conv_size,activation,strides=[1,1],padding="SAME"):
+    def __init__(self,input_dim,out_dim,conv_size,activation,out_shape,strides=[1,1],padding="SAME"):
         assert len(conv_size) == 2,"incorrect conv size"
-        out_shape = conv_size+[out_dim]+[input_dim]
-        init_vals = tf.initializers.glorot_normal()(out_shape)
+        filter_shape = conv_size+[out_dim]+[input_dim]
+        init_vals = tf.initializers.glorot_normal()(filter_shape)
         self.weights = tf.Variable(init_vals,name="weights")
         #self.biases = tf.Variable(tf.ones(out_dim)*0.01,name="biases")
         self.activation = activation
         self.strides = strides
         self.padding = padding
         self.out_dim = out_dim
+        self.out_shape = out_shape
+
 
     def calc(self,input_vec):
-        out_shape = input_vec.get_shape().as_list()
-        print(out_shape)
-        out_shape[1] *= self.strides[0]
-        out_shape[2] *= self.strides[1]
-        out_shape[3] = self.out_dim
+        in_shape = input_vec.get_shape().as_list()
+        out_shape = [
+            in_shape[0],
+            self.out_dim,
+            self.out_shape[0],
+            self.out_shape[1],
+        ]
         linval = tf.nn.conv2d_transpose(
             value=input_vec,
             filter=self.weights,
             output_shape=out_shape,
-            strides=[1]+self.strides+[1],
+            strides=self.strides,
             data_format=FORMAT)
         #affine_val = linval + self.biases
         activated = (linval if self.activation is None else
@@ -105,20 +136,20 @@ class Convpool2:
         self.POOL_SHAPE = [2,2]
         self.out_activ = out_activ
         self.use_batchnorm = use_batchnorm
-        self.bn1 = tf.layers.BatchNormalization(momentum=0.9)
-        if self.use_batchnorm:
-            self.bn2 = tf.layers.BatchNormalization(momentum=0.9)
+        #self.bn1 = tf.layers.BatchNormalization(momentum=0.9)
+        #if self.use_batchnorm:
+        #    self.bn2 = tf.layers.BatchNormalization(momentum=0.9)
         self.conv1 = Conv2d(in_dim,out_dim,self.CONV_SIZE,None)
         self.conv2 = Conv2d(out_dim,out_dim,self.CONV_SIZE,None,strides=self.POOL_SHAPE)
 
     def calc(self,in_vec):
         cur_vec = in_vec
         cur_vec = self.conv1.calc(cur_vec)
-        cur_vec = self.bn1(cur_vec)
+        #cur_vec = self.bn1(cur_vec)
         cur_vec = default_activ(cur_vec)
         cur_vec = self.conv2.calc(cur_vec)
-        if self.use_batchnorm:
-            cur_vec = self.bn2(cur_vec)*0.1
+        #if self.use_batchnorm:
+        #    cur_vec = self.bn2(cur_vec)
         if self.out_activ is not None:
             cur_vec = self.out_activ(cur_vec)
         #cur_vec = avgpool2d(cur_vec,self.POOL_SHAPE)
@@ -126,11 +157,11 @@ class Convpool2:
 
 
 class Deconv2:
-    def __init__(self,in_dim,out_dim,out_activ):
+    def __init__(self,in_dim,out_dim,out_activ,out_shape):
         self.CONV_SIZE = [3,3]
         self.POOL_SHAPE = [2,2]
-        self.conv1 = ConvTrans2d(in_dim,in_dim,self.CONV_SIZE,default_activ,strides=self.POOL_SHAPE)
-        self.conv2 = ConvTrans2d(in_dim,out_dim,self.CONV_SIZE,out_activ)
+        self.conv1 = ConvTrans2d(in_dim,in_dim,self.CONV_SIZE,default_activ,out_shape,strides=self.POOL_SHAPE)
+        self.conv2 = ConvTrans2d(in_dim,out_dim,self.CONV_SIZE,out_activ,out_shape)
 
     def calc(self,in_vec):
         cur_vec = in_vec
@@ -183,7 +214,7 @@ def assign_moving_average(var,cur_val,decay):
 
 class QuantBlock:
     def __init__(self,QUANT_SIZE,NUM_QUANT,QUANT_DIM):
-        init_vals = tf.random_normal([NUM_QUANT,QUANT_SIZE,QUANT_DIM],dtype=tf.float32)*0.1
+        init_vals = tf.random_normal([NUM_QUANT,QUANT_SIZE,QUANT_DIM],dtype=tf.float32)
         self.vectors = tf.Variable(init_vals,name="vecs")
         self.vector_counts = tf.Variable(tf.zeros(shape=[NUM_QUANT,QUANT_SIZE],dtype=tf.float32),name="vecs")
 
@@ -194,7 +225,7 @@ class QuantBlock:
         self.QUANT_SIZE = QUANT_SIZE
         self.QUANT_DIM = QUANT_DIM
         self.NUM_QUANT = NUM_QUANT
-        self._decay = 0.5
+        self._decay = 0.9
         self._epsilon=1e-5
 
     def calc(self, input):
@@ -263,13 +294,14 @@ class QuantBlock:
         return commitment_loss ,combined_update
 
     def resample_bad_vecs(self):
-        #sample_vals = tf.random_normal([self.QUANT_SIZE,self.QUANT_DIM],dtype=tf.float32)
-        #equal_vals = tf.cast(tf.equal(self.vector_counts,0),dtype=tf.float32)
-        #equal_vals= tf.reshape(equal_vals,shape=[self.QUANT_SIZE,1])
-        #new_vecs = self.vectors - self.vectors * equal_vals + sample_vals * equal_vals
-        #vec_assign = tf.assign(self.vectors,new_vecs)
+        sample_vals = tf.random_normal([self.NUM_QUANT,self.QUANT_SIZE,self.QUANT_DIM],dtype=tf.float32)
+        equal_vals = tf.cast(tf.equal(self.vector_counts,0),dtype=tf.float32)
+        equal_vals= tf.reshape(equal_vals,shape=[self.NUM_QUANT,self.QUANT_SIZE,1])
+        new_vecs = self.vectors - self.vectors * equal_vals + sample_vals * equal_vals
+        vec_assign = tf.assign(self.vectors,new_vecs)
+        ema_assign = tf.assign(self._ema_w,new_vecs)
         zero_assign = tf.assign(self.vector_counts,tf.zeros_like(self.vector_counts))
-        #tot_assign = tf.group([vec_assign,zero_assign])
+        tot_assign = tf.group([vec_assign,zero_assign,ema_assign])
         return zero_assign#tot_assign
 
 def prod(l):
@@ -280,44 +312,123 @@ def prod(l):
 
 class QuantBlockImg(QuantBlock):
     def calc(self,input):
+        input = tf.transpose(input,perm=(0,2,3,1))
         in_shape = input.get_shape().as_list()
         flat_val = tf.reshape(input,[prod(in_shape[:3]),in_shape[3]])
         out,o1,o2 = QuantBlock.calc(self,flat_val)
         restored = tf.reshape(out,in_shape)
+        restored = tf.transpose(restored,perm=(0,3,1,2))
         return restored,o1,o2
 
+IMG_LEVEL = 32
+SECOND_LEVEL = 64
+THIRD_LEVEL = 128
+FOURTH_LEVEL = 192
+FIFTH_LEVEL = 256
+ZIXTH_LEVEL = 256
 class MainCalc:
     def __init__(self):
-        self.convpool1 = Convpool2(3,64,default_activ)
-        self.convpool2 = Convpool2(64,128,None)
-        self.quant_block = QuantBlockImg(64,4,32)
-        self.convunpool1 = Deconv2(128,64,default_activ)
-        self.convunpool2 = Deconv2(64,3,tf.nn.sigmoid)
+        self.convpool1 = Convpool2(3,IMG_LEVEL,default_activ)
+        self.convpool2 = Convpool2(IMG_LEVEL,SECOND_LEVEL,None)
+        self.convpool3 = Convpool2(SECOND_LEVEL,THIRD_LEVEL,default_activ)
+        self.convpool4 = Convpool2(THIRD_LEVEL,FOURTH_LEVEL,None)
+        self.convpool5 = Convpool2(FOURTH_LEVEL,FIFTH_LEVEL,default_activ)
+        self.convpool6 = Convpool2(FIFTH_LEVEL,ZIXTH_LEVEL,None)
 
+        self.quanttrans1 = Conv1x1(SECOND_LEVEL,SECOND_LEVEL,None)
+        self.quant_block1 = QuantBlockImg(256,1,SECOND_LEVEL)
+        self.quanttrans2 = Conv1x1(FOURTH_LEVEL,FOURTH_LEVEL,None)
+        self.quant_block2 = QuantBlockImg(256,4,FOURTH_LEVEL//4)
+        self.quant_block3 = QuantBlockImg(256,4,ZIXTH_LEVEL//4)
+
+        self.deconv6 = Deconv2(ZIXTH_LEVEL,FIFTH_LEVEL,default_activ,get_out_shape(6))
+        self.deconv5 = Deconv2(FIFTH_LEVEL,FOURTH_LEVEL,default_activ,get_out_shape(5))
+        self.deconv4 = Deconv2(FOURTH_LEVEL,THIRD_LEVEL,default_activ,get_out_shape(4))
+        self.deconv3 = Deconv2(THIRD_LEVEL,SECOND_LEVEL,default_activ,get_out_shape(3))
+        self.deconv2 = Deconv2(SECOND_LEVEL,IMG_LEVEL,default_activ,get_out_shape(2))
+        self.deconv1 = Deconv2(IMG_LEVEL,3,tf.sigmoid,get_out_shape(1))
+
+        self.quantdownsample32 = Conv1x1Upsample(ZIXTH_LEVEL,FOURTH_LEVEL,None,get_out_shape(5),4)
+        self.quantdownsample31 = Conv1x1Upsample(ZIXTH_LEVEL,SECOND_LEVEL,None,get_out_shape(3),16)
+        self.quantdownsample21 = Conv1x1Upsample(FOURTH_LEVEL,SECOND_LEVEL,None,get_out_shape(3),4)
+
+        self.bn1a = tf.layers.BatchNormalization(axis=1)
+        self.bn1b = tf.layers.BatchNormalization(axis=1)
+        self.bn2a = tf.layers.BatchNormalization(axis=1)
+        self.bn2b = tf.layers.BatchNormalization(axis=1)
+        self.bn3 = tf.layers.BatchNormalization(axis=1)
+        #self.deconvbn2 = tf.layers.BatchNormalization()
 
     def calc(self,input):
         out1 = self.convpool1.calc(input)
         out2 = self.convpool2.calc(out1)
-        quant,quant_loss,update = self.quant_block.calc(out2)
-        print(quant.shape)
-        dec1 = self.convunpool1.calc(quant)
-        decoded_final = self.convunpool2.calc(dec1)
+        out3 = self.convpool3.calc(out2)
+        out4 = self.convpool4.calc(out3)
+        out5 = self.convpool5.calc(out4)
+        out6 = self.convpool6.calc(out5)
+
+        quant3,quant_loss3,update3 = self.quant_block3.calc((self.bn3(out6,training=True)*0.5))
+        dec6 = self.deconv6.calc(quant3)
+        dec5 = self.deconv5.calc(dec6)
+        out4trans = self.quanttrans2.calc(out4)
+
+        quant2,quant_loss2,update2 = self.quant_block2.calc(((self.bn1a(dec5,training=True)+self.bn1b(out4trans,training=True)))*0.5)
+        dec4 = self.deconv4.calc(quant2+self.quantdownsample32.calc(quant3))
+        dec3 = self.deconv3.calc(dec4)
+        out2trans = self.quanttrans1.calc(out2)
+        quant1,quant_loss1,update1 = self.quant_block1.calc(self.bn2a(dec3,training=True)+self.bn2b(out2trans,training=True))
+        dec2 = self.deconv2.calc(quant1+self.quantdownsample21.calc(quant2)+self.quantdownsample31.calc(quant3))
+        dec1 = self.deconv1.calc(dec2)
+        decoded_final = dec1
 
         reconstr_loss = tf.reduce_sum(sqr(decoded_final - input))
 
+        quant_loss = quant_loss1 + quant_loss2 + quant_loss3
         tot_loss = reconstr_loss + quant_loss
-        return update,tot_loss, reconstr_loss,decoded_final
+        tot_update = tf.group([update1,update2,update3])
+        #losest_list = [closest1]#,closest2,closest3]
+        return tot_update,tot_loss, reconstr_loss,decoded_final
+
+    # def __init__(self):
+    #     self.convpool1 = Convpool2(3,64,default_activ)
+    #     self.convpool2 = Convpool2(64,128,None)
+    #     self.quant_block = QuantBlockImg(64,4,32)
+    #     self.convunpool1 = Deconv2(128,64,default_activ)
+    #     self.convunpool2 = Deconv2(64,3,tf.nn.sigmoid)
+    #
+    #
+    # def calc(self,input):
+    #     out1 = self.convpool1.calc(input)
+    #     out2 = self.convpool2.calc(out1)
+    #     quant,quant_loss,update = self.quant_block.calc(out2)
+    #     print(quant.shape)
+    #     dec1 = self.convunpool1.calc(quant)
+    #     decoded_final = self.convunpool2.calc(dec1)
+    #
+    #     reconstr_loss = tf.reduce_sum(sqr(decoded_final - input))
+    #
+    #     tot_loss = reconstr_loss + quant_loss
+    #     return update,tot_loss, reconstr_loss,decoded_final
 
     def periodic_update(self):
-        return self.quant_block.resample_bad_vecs()
+        return tf.group([
+            self.quant_block1.resample_bad_vecs(),
+            self.quant_block2.resample_bad_vecs(),
+            self.quant_block3.resample_bad_vecs(),
+        ])
 
 mc = MainCalc()
-place = tf.placeholder(shape=[BATCH_SIZE,200,320,3],dtype=tf.float32)
+place = tf.placeholder(shape=[BATCH_SIZE,3,200,320],dtype=tf.float32)
 
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
 
 mc_update, loss, reconst_l, final_output = mc.calc(place)
 resample_update = mc.periodic_update()
+
+batchnorm_updates = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+print(batchnorm_updates)
+comb_updates = tf.group(batchnorm_updates)
+tot_update = tf.group([mc_update,comb_updates])
 
 opt = optimizer.minimize(loss)
 orig_imgs = []
@@ -325,7 +436,9 @@ orig_filenames = []
 for img_name in os.listdir("data/input_data"):
     with Image.open("data/input_data/"+img_name) as img:
         if img.mode == "RGB":
-            orig_imgs.append(np.array(img).astype(np.float32)/256.0)
+            arr = np.array(img)
+            arr = np.transpose(arr,(2,0,1))
+            orig_imgs.append(arr.astype(np.float32)/256.0)
             orig_filenames.append(img_name)
 
 fold_names = [fname.split('.')[0]+"/" for fname in orig_filenames[:50]]
@@ -348,6 +461,7 @@ config.gpu_options.allow_growth=True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
     print_num = 0
+    lossval_num = 0
     if os.path.exists(SAVE_DIR+"checkpoint"):
         print("reloaded")
         checkpoint = tf.train.latest_checkpoint(SAVE_DIR)
@@ -367,7 +481,7 @@ with tf.Session(config=config) as sess:
                 batch.append(img)
                 if len(batch) == BATCH_SIZE:
                     batch_count += 1
-                    _,_,cur_loss,cur_rec = sess.run([mc_update,opt,loss,reconst_l],feed_dict={
+                    _,_,cur_loss,cur_rec = sess.run([tot_update,opt,loss,reconst_l],feed_dict={
                         place:np.stack(batch)
                     })
                     loss_count += 1
@@ -378,7 +492,13 @@ with tf.Session(config=config) as sess:
                     EPOC_SIZE = 100
                     if batch_count % EPOC_SIZE == 0:
                         print("epoc ended, loss: {}   {}".format(tot_loss/loss_count,rec_loss/loss_count))
-                        logfile.write(",".join([str(val) for val in sess.run(mc.quant_block.vector_counts)])+"\n")
+                        lossval_num += 1
+                        logfile.write("counts step {} quant 1".format(lossval_num))
+                        logfile.write(",".join([str(val.astype(np.int64)) for val in sess.run(mc.quant_block1.vector_counts)])+"\n")
+                        logfile.write("counts step {} quant 2".format(lossval_num))
+                        logfile.write(",".join([str(val.astype(np.int64)) for val in sess.run(mc.quant_block2.vector_counts)])+"\n")
+                        logfile.write("counts step {} quant 3".format(lossval_num))
+                        logfile.write(",".join([str(val.astype(np.int64)) for val in sess.run(mc.quant_block3.vector_counts)])+"\n")
                         logfile.flush()
                         sess.run(resample_update)
 
@@ -402,6 +522,7 @@ with tf.Session(config=config) as sess:
                                     pixel_vals = (batch_outs * 256).astype(np.uint8)
                                     for out,out_fold in zip(pixel_vals,fold_batch):
                                         #print(out.shape)
+                                        out = np.transpose(out,(1,2,0))
                                         img = Image.fromarray(out)
                                         img_path = "data/result/{}{}.jpg".format(out_fold,print_num)
                                         #print(img_path)
